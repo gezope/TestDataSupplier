a560ab0f4888c8589378f442352f091c73bb1195
Zoltan Gero <gezope@gmail.com>
2011-03-11 20:24:09 +0100

    licence added

--- testdatasupplier.php
+++ testdatasupplier.php
@@ -6,6 +6,23 @@ Description: This plugin enables you to supply lots of data for testing your Wor
 Version: 0.1alpha
 Author: Apricog, WPMonks, gricso and gezope
 Author URI: http://wpmonks.com
-License: GPL licence
+License: GPL2.0 licence
 */
+
+/*  Copyright 2011  Apricog  (email : admin@apricog.com, gricso@wpmonks.com, gezope@wpmonks.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 ?>
