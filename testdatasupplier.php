ca36a8f4f4b47cf6fdebc2adb56ae2cd5b63ac04
Zoltan Gero <gezope@gmail.com>
2011-03-11 20:17:35 +0100

    valid header created

---
+++ testdatasupplier.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: TestDataSupplier
+Plugin URI: http://apricog.com
+Description: This plugin enables you to supply lots of data for testing your WordPress without a big efford.
+Version: 0.1alpha
+Author: Apricog, WPMonks, gricso and gezope
+Author URI: http://wpmonks.com
+License: MIT licence
+*/
+?>
